6f64666b1bd0157b5025a6d6d22ac85b5ee8ec23
cultab <rroarck@gmail.com>
2020-04-28 14:47:26 +0300

    fully merged

---
+++ papercolor-dark.h
@@ -0,0 +1,28 @@
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"#262626",
+	"#df0000",
+	"#5faf00",
+	"#dfaf05",
+	"#303030",
+	"#578787",
+	"#df875f",
+	"#d0d0d0",
+
+	/* 8 bright colors */
+	"#8a8a8a",
+	"#5faf5f",
+	"#afdf0d",
+	"#af87d7",
+	"#585858",
+	"#ff5faf",
+	"#00afaf",
+	"#5fafdf",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#d7d7d7",
+	"#cccccc",
+	"#1c1c1c",
+};
